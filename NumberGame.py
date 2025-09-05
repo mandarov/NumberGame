8dacdecee06c44b740cc9ea122bad9e0c12f54ab
student1 <student1@class.local>
2025-09-05 09:47:56 +0900

    Угадай число

---
+++ NumberGame.py
@@ -0,0 +1,15 @@
+import random
+
+n = random.randint(1, 100)
+guess = int(input("Введите число: "))
+while n!= guess:
+    if guess < n:
+        print("Меньше")
+        guess = int(input("Напишите число заново: "))
+    elif guess > n:
+        print("Больше")
+        guess = int(input("Напишите число заново: "))
+    else:
+        break
+    print("Правильно")
+    break
