a5fa61492fa9fb91033f2870c253dcfb28981a53
student1 <student1@class.local>
2025-10-03 08:59:05 +0900

    Шаблон

--- NumberGame.py
+++ NumberGame.py
@@ -3,13 +3,13 @@ import random
 n = random.randint(1, 100)
 guess = int(input("Введите число: "))
 while n!= guess:
-    if guess < n:
-        print("Меньше")
-        guess = int(input("Напишите число заново: "))
+    if guess > n:
+        print("")
+        guess = int(input(" "))
     elif guess > n:
         print("Больше")
-        guess = int(input("Напишите число заново: "))
+        guess = int(input(" "))
     else:
         break
-    print("Правильно")
+    print("")
     break
