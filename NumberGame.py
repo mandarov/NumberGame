4654ed3a451bd3aa02c4861f6d4206c09d9fc22a
student1 <student1@class.local>
2025-10-03 09:19:55 +0900

    Грит

--- NumberGame.py
+++ NumberGame.py
@@ -1,15 +1,15 @@
 import random
-
+#
 n = random.randint(1, 100)
 guess = int(input("Введите число: "))
 while n!= guess:
-    if guess > n:
-        print("")
-        guess = int(input(" "))
+    if guess < n:
+        print("Меньше")
+        guess = int(input("Напишите число заново: "))
     elif guess > n:
         print("Больше")
-        guess = int(input(" "))
+        guess = int(input("Напишите число заново: "))
     else:
         break
-    print("")
+    print("Правильно")
     break
